6c6bee588340f50071b7cef0c467c59c87ea5cf1
pocj8ur4in <pocj8ur4in@haii.io>
2025-10-10 15:24:16 +0900

    [BOJ/Bronze 5] 10926. ??!

---
+++ boj/10926.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cout.tie(NULL);
+    cin.tie(NULL);
+
+    string str;
+    cin >> str;
+    cout << str << "??" "!";
+}
