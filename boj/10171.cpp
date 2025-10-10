54743198b30a3c9d3694e41b25f05f2ce609753d
pocj8ur4in <pocj8ur4in@haii.io>
2025-10-10 15:23:03 +0900

    [BOJ/Bronze 5] 10171. 고양이

---
+++ boj/10171.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cout.tie(NULL);
+    cin.tie(NULL);
+
+    cout << "\\    /\\\n";
+    cout << " )  ( ')\n";
+    cout << "(  /  )\n";
+    cout << " \\(__)|\n";
+
+    return 0;
+}
