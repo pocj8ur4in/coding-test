78dfd06bd5dd5b2fa344e646e8befb7065b11320
pocj8ur4in <pocj8ur4in@haii.io>
2025-11-17 01:34:16 +0900

    [BOJ/Sliver 5] 11651. 좌표 정렬하기 2

---
+++ boj/11651.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N;
+    cin >> N;
+
+    vector<pair<int, int>> points;
+
+    for(int i = 0; i < N; i++) {
+        int x, y;
+        cin >> x >> y;
+
+        points.push_back({x, y});
+    }
+
+    sort(points.begin(), points.end(), greater<vector<pair<int, int>>>());
+
+    for (int i = 0; i < N; i++) {
+        cout << points[i].first << " " << points[i].second << "\n";
+    }
+
+    return 0;
+}
