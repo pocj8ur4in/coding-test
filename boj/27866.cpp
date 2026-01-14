e3cb11a31dfed395316ef93606827ad781cc4825
pocj8ur4in <pocj8ur4in@gmail.com>
2026-01-14 10:19:41 +0900

    [BOJ/Bronze 5] 27866. 문자와 문자열

---
+++ boj/27866.cpp
@@ -0,0 +1,20 @@
+#include <ios>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio();
+    cout.tie(NULL);
+    cin.tie(NULL);
+
+    string S;
+    int i;
+
+    cin >> S >> i;
+
+    cout << S[i - 1];
+
+    return 0;
+}
